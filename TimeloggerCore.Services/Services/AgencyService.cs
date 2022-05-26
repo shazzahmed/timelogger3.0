d71e12c9d475d64a498eda584b06406e7493d82a
shahzhob-itsol-2019 <shahzhob@itsolution24x7.com>
2022-05-26 16:25:02 +0500

    Revert "Agency changes without communication"
    This reverts commit 76fa75b866ea92868073c5c3d8bee7332d0afbff.

--- TimeloggerCore.Services/Services/AgencyService.cs
+++ TimeloggerCore.Services/Services/AgencyService.cs
@@ -8,123 +8,45 @@ using System.Text;
 using TimeloggerCore.Common.Models;
 using System.Threading.Tasks;
 using ApplicationUser = TimeloggerCore.Data.Entities.ApplicationUser;
-using TimeloggerCore.Core.ISecurity;
-//using TimeloggerCore.Services.ICommunication;
-using static TimeloggerCore.Common.Utility.Enums;
-using TimeloggerCore.Common.Options;
-using Microsoft.Extensions.Options;
 
 namespace TimeloggerCore.Services
 {
     public class AgencyService : BaseService<ClientAgencyModel, ClientAgency, int>, IAgencyService
     {
         private readonly IClientAgencyRepository _clientAgencyRepository;
-        private readonly ISecurityService _securityService;
-        private readonly INotificationTemplateService _notificationTemplateService;
-        //private readonly ICommunicationService _communicationService;
-        private readonly TimeloggerCoreOptions _timeloggerCoreOptions;
 
-        public AgencyService(IMapper mapper, IClientAgencyRepository clientAgencyRepository, ISecurityService securityService, INotificationTemplateService notificationTemplateService/*, ICommunicationService communicationService*/, IOptionsSnapshot<TimeloggerCoreOptions> timeloggerCoreOptions, IUnitOfWork unitOfWork) : base(mapper, clientAgencyRepository, unitOfWork)
+        public AgencyService(IMapper mapper, IClientAgencyRepository clientAgencyRepository, IUnitOfWork unitOfWork) : base(mapper, clientAgencyRepository, unitOfWork)
         {
             _clientAgencyRepository = clientAgencyRepository;
-            _securityService = securityService;
-            _notificationTemplateService = notificationTemplateService;
-            //_communicationService = communicationService;
-            _timeloggerCoreOptions = timeloggerCoreOptions.Value;
-
         }
-        public async Task<BaseModel> GetAgencyClients(string userId)
+        public Task<List<ClientAgency>> GetAgencyClients(string userId)
         {
-            var result = await _clientAgencyRepository.GetAgencyClients(userId);
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<List<ClientAgency>, List<ClientAgencyModel>>(result)
-            };
+            return _clientAgencyRepository.GetAgencyClients(userId);
         }
 
-        public async Task<BaseModel> GetAgencyEmployee(string AgencyId)
+        public Task<List<ApplicationUser>> GetAgencyEmployee(string AgencyId)
         {
-            var result = await _clientAgencyRepository.GetAgencyEmployee(AgencyId);
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<List<ApplicationUser>, List<ApplicationUserModel>>(result)
-            };
+            return _clientAgencyRepository.GetAgencyEmployee(AgencyId);
         }
 
-        public async Task<BaseModel> GetAllWorker()
+        public Task<List<ApplicationUser>> GetAllWorker()
         {
-            var result = await _clientAgencyRepository.GetAllWorker();
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<List<ApplicationUser>, List<ApplicationUserModel>>(result)
-            };
+            return _clientAgencyRepository.GetAllWorker();
         }
 
-        public async Task<BaseModel> GetClientAgencies(string userId)
+        public Task<List<ClientAgency>> GetClientAgencies(string userId)
         {
-            var result = await _clientAgencyRepository.GetClientAgencies(userId);
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<List<ClientAgency>, List<ClientAgencyModel>>(result)
-            };
+            return _clientAgencyRepository.GetClientAgencies(userId);
         }
 
-        public async Task<BaseModel> GetClientAgency(ClientAgencyModel clientAgencyModel)
+        public Task<ClientAgency> GetClientAgency(AgencyAlreadyExitModel agencyAlreadyExit)
         {
-            var result = await _clientAgencyRepository.GetClientAgency(clientAgencyModel);
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<ClientAgency, ClientAgencyModel>(result)
-            };
+            return _clientAgencyRepository.GetClientAgency(agencyAlreadyExit);
         }
 
-        public async Task<BaseModel> GetSingleClientAgencies(int Id)
+        public Task<ClientAgency> GetSingleClientAgencies(int Id)
         {
-            var result = await _clientAgencyRepository.GetSingleClientAgencies(Id);
-            return new BaseModel
-            {
-                Success = true,
-                Data = mapper.Map<ClientAgency, ClientAgencyModel>(result)
-            };
-        }
-        public async Task<BaseModel> AddClientAgency(ClientAgencyModel clientAgencyModel)
-        {
-            ClientAgency clientAgency = await _clientAgencyRepository.GetClientAgency(clientAgencyModel);
-            if (clientAgency == null)
-            {
-                var isUserExit = await _securityService.GetUserDetail(clientAgencyModel.AgencyId);
-                var userInfo = (UserInfo)isUserExit.Data;
-                clientAgencyModel.AgencyId = userInfo.Id;
-                clientAgencyModel.IsActive = false;
-                var userCreated = await Add(clientAgencyModel);
-                if (userCreated.Id > 1)
-                {
-                    var template = await _notificationTemplateService.GetNotificationTemplate(NotificationTemplates.ConfirmClientAgency, NotificationTypes.Email);
-                    var emailMessage = template.MessageBody.Replace("#Name", $"{userInfo.FirstName} { userInfo.LastName}")
-                                                            .Replace("#Link", $"{_timeloggerCoreOptions.ApiUrl}?{clientAgencyModel.Id}")
-                                                            .Replace("#ClientName", $"{userInfo.FirstName} ({ userInfo.LastName})");
-                    //var sent = await _communicationService.SendEmail(template.Subject, emailMessage, userInfo.Email);
-                    //await SendEmail(clientAgency.Id, clientAgency.Agency.FullName, clientAgency.Agency.FirstName, clientAgency.Agency.LastName, clientAgency.Client.FullName, clientAgency.Client.Email, clientAgency.Agency.Email);
-                    return new BaseModel
-                    {
-                        Success = true,
-                        Data = userCreated,
-                        Message = "Successfully sent invitation to agency."
-
-                    };
-                }
-            }
-            return new BaseModel
-            {
-                Success = true,
-                Data = clientAgency,
-                Message = "Already Exist."
-            };
+            return _clientAgencyRepository.GetSingleClientAgencies(Id);
         }
     }
 }
